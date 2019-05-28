e7d345fcd6405fd52cbfeb34ae3632c307b962f1
Sandesh Kota <sandeshjkota@gmail.com>
2019-05-28 13:34:08 +0530

    Create LinkedListStack.cs

---
+++ DataStructure/Stack/LinkedListStack.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsTryWhenWeAreAlive
+{
+    public class LinkedListStack<T> : IEnumerable<T>
+    {
+
+        private LinkedList<T>  _list = new LinkedList<T>();
+
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (_list.Count <= 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+            
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count <= 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
