2207b032bf1f3f3747eaacf0db51f51f7560e34e
Sandesh Kota <sandeshjkota@gmail.com>
2019-05-28 17:26:38 +0530

    Update LinkedListQueue.cs

--- DataStructure/Queue/LinkedListQueue.cs
+++ DataStructure/Queue/LinkedListQueue.cs
@@ -1,1 +1,61 @@
-sa
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsTryWhenWeAreAlive
+{
+    public class LinkedListQueue<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_list.Count <= 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+
+            var value = _list.First.Value;
+            _list.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count <= 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
